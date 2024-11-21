16861cf1c873e4a2d1b7e34fdc882386f78fd530
Tomás Santos <72564366+tmmss1-iscte@users.noreply.github.com>
2024-11-21 15:57:31 +0000

    Add files via upload

---
+++ env_variables.py
@@ -0,0 +1,12 @@
+# Make a copy and rename it as env_variables.py file
+
+
+# input
+db_filepath = "/home/kali/Desktop/InformationElementsAnalyser/DB/InformationElements.db"         # input database filepath
+
+# arguments
+show_variation = 'show-bits-variation'                                                           # ['show-bits-variation' or 'show-bytes-variation' or 'no-variation']
+
+# output
+write_to_file = False                                                                             # write ouput to text file ['True' or 'False']
+txt_filepath = "/home/kali/Desktop/InformationElementsAnalyser/InformationElementsReport.txt"    # output .txt file filepath
